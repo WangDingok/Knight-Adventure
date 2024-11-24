e33bf0259214e8abb66c8aef1baeb2ca06c33152
Đào Xuân Duy <daoxuanduy@Mac-cua-Duy.local>
2024-11-24 21:36:29 +0700

    upgrade: boss move to player and acttack

--- Game/Assets/codes/boss_run.cs
+++ Game/Assets/codes/boss_run.cs
@@ -2,17 +2,33 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class boss_run : MonoBehaviour
+public class Boss_run : MonoBehaviour
 {
+    public float speed = 2.5f;
+    public float rangeAttack = 2.5f;
+    Rigidbody2D rb;
+    Boss boss;
+    Transform player;
+    Animator animator;
     // Start is called before the first frame update
     void Start()
     {
-        
+       player = GameObject.FindGameObjectWithTag("Player").transform;
+       rb = GetComponent<Rigidbody2D>();
+       boss = GetComponent<Boss>();
+       animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector2 target = new Vector2(player.position.x, rb.position.y);
+        Vector2 newPos = Vector2.MoveTowards(rb.position, target, speed * Time.fixedDeltaTime);
+        rb.MovePosition(newPos);
+        boss.LookAtPlayer();
+        if (Vector2.Distance(player.position, rb.position) <= rangeAttack)
+        {
+            animator.SetTrigger("Acttack");
+        }
     }
 }
