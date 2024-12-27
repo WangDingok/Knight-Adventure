3bea3fed1f6006a9bfcb9978813ad47274e6995a
WangDingok <doanphucduoc2@gmail.com>
2024-12-27 17:14:55 +0700

    fix bug

--- Game/Assets/codes/Boss/boss_run1.cs
+++ Game/Assets/codes/Boss/boss_run1.cs
@@ -5,33 +5,41 @@ using UnityEngine;
 public class Boss_run : MonoBehaviour
 {
     public float speed = 7f;
-    public float rangeAttack = 2.5f;
+    public float rangeAttack;
+    public float range_see_player;
     Rigidbody2D rb;
     Boss boss;
     Transform player;
     Animator animator;
-    BossHealth bossHealth;
+    heath bossHealth;
     // Start is called before the first frame update
     void Start()
     {
-       player = GameObject.FindGameObjectWithTag("Player").transform;
-       rb = GetComponent<Rigidbody2D>();
-       boss = GetComponent<Boss>();
-       animator = GetComponent<Animator>();
-       bossHealth = GetComponent<BossHealth>();
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        rb = GetComponent<Rigidbody2D>();
+        boss = GetComponent<Boss>();
+        animator = GetComponent<Animator>();
+        bossHealth = GetComponent<heath>();
     }
 
     // Update is called once per frame
     void Update()
     {
         Vector2 target = new Vector2(player.position.x, rb.position.y);
-        Vector2 newPos = Vector2.MoveTowards(rb.position, target, speed * Time.fixedDeltaTime);
-        rb.MovePosition(newPos);
-        boss.LookAtPlayer();
         float rangetoPlayer = Mathf.Abs(Vector2.Distance(player.position, rb.position));
-            animator.SetBool("attack_v2", rangetoPlayer <= rangeAttack & bossHealth.health<=20f);
-            
-            animator.SetBool("attack", rangetoPlayer <= rangeAttack & bossHealth.health>20f);
+
+        if (rangetoPlayer < range_see_player)
+        {
+            Vector2 newPos = Vector2.MoveTowards(rb.position, target, speed * Time.fixedDeltaTime);
+            rb.MovePosition(newPos);
+        }
+        animator.SetBool("walk", rangetoPlayer < range_see_player);
+
+        boss.LookAtPlayer();
+        //float rangetoPlayer = Mathf.Abs(Vector2.Distance(player.position, rb.position));
+        animator.SetBool("attack_v2", rangetoPlayer <= rangeAttack & bossHealth.current_heath <= 20f);
+
+        animator.SetBool("attack", rangetoPlayer <= rangeAttack & bossHealth.current_heath > 20f);
 
     }
 }
